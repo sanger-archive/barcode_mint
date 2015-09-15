21ad8ff0c9f68c322457ce793cf6a872a12898ae
Ronan Forman <rf9@sanger.ac.uk>
2015-09-15 17:13:36 +0100

    No longer generates a barcode if there are other errors with the query. (No longer eats up barcode space without using them)

--- barcode/views.py
+++ barcode/views.py
@@ -13,55 +13,124 @@ def source_list(request):
     return HttpResponse(json.dumps({'sources': [source.name for source in Source.objects.all()]}))
 
 
-@atomic
+#
+# @atomic
+# @csrf_exempt
+# def register(request):
+#     errors = []
+#
+#     source_string = request.REQUEST.get('source').lower()
+#     sources = Source.objects.filter(name=source_string)
+#     if sources.count() == 1:
+#         source = sources[0]
+#     else:
+#         errors.append("unknown source")
+#         source = None
+#
+#     barcode_string = request.REQUEST.get('barcode')
+#     if not barcode_string:
+#         barcode_string = source_string + str(NumberGenerator.objects.create().id)
+#
+#     barcode_string = barcode_string.upper().strip()
+#
+#     if Barcode.objects.filter(barcode=barcode_string).count() > 0:
+#         errors.append("barcode already registered")
+#
+#     uuid_string = request.REQUEST.get('uuid')
+#     uuid = None
+#     if uuid_string:
+#         try:
+#             uuid = UUID(uuid_string)
+#         except ValueError:
+#             errors.append("malformed uuid")
+#
+#     if Barcode.objects.filter(uuid=uuid).count() > 0:
+#         errors.append("uuid already registered")
+#
+#     if len(errors) == 0:
+#         if not uuid:
+#             barcode = Barcode.objects.create(barcode=barcode_string, source=source)
+#         else:
+#             barcode = Barcode.objects.create(barcode=barcode_string, source=source, uuid=uuid)
+#
+#         return HttpResponse(json.dumps({
+#             'source': barcode.source.name,
+#             'barcode': barcode.barcode,
+#             'uuid': str(barcode.uuid),
+#             'errors': errors,
+#         }))
+#     else:
+#         return HttpResponse(json.dumps({
+#             'source': source_string,
+#             'barcode': barcode_string,
+#             'uuid': uuid,
+#             'errors': errors,
+#         }), status=422)
+
 @csrf_exempt
 def register(request):
     errors = []
 
-    source_string = request.REQUEST.get('source').lower()
-    sources = Source.objects.filter(name=source_string)
-    if sources.count() == 1:
-        source = sources[0]
-    else:
-        errors.append("unknown source")
-        source = None
-
     barcode_string = request.REQUEST.get('barcode')
-    if not barcode_string:
-        barcode_string = source_string + str(NumberGenerator.objects.create().id)
 
-    barcode_string = barcode_string.upper().strip()
+    if barcode_string:
+        barcode_string = barcode_string.upper().strip()
+        if len(barcode_string) < 5:
+            errors.append("barcode too short")
+        if Barcode.objects.filter(barcode=barcode_string).count() > 0:
+            errors.append("barcode already taken")
 
-    if Barcode.objects.filter(barcode=barcode_string).count() > 0:
-        errors.append("barcode already registered")
+    source_string = request.REQUEST.get('source')
+    source = None
+    if not source_string:
+        errors.append("source missing")
+    else:
+        sources = Source.objects.filter(name=source_string.lower().strip())
+        if sources.count() == 1:
+            source = sources[0]
+        else:
+            errors.append("invalid source")
 
     uuid_string = request.REQUEST.get('uuid')
     uuid = None
     if uuid_string:
         try:
             uuid = UUID(uuid_string)
-        except ValueError:
-            errors.append("malformed uuid")
 
-    if Barcode.objects.filter(uuid=uuid).count() > 0:
-        errors.append("uuid already registered")
+            if Barcode.objects.filter(uuid=uuid).count() > 0:
+                errors.append("uuid already taken")
+
+        except ValueError:
+            errors.append('malformed uuid')
 
     if len(errors) == 0:
-        if not uuid:
-            barcode = Barcode.objects.create(barcode=barcode_string, source=source)
-        else:
-            barcode = Barcode.objects.create(barcode=barcode_string, source=source, uuid=uuid)
+        barcode = generate_barcode(barcode_string, uuid, source)
 
         return HttpResponse(json.dumps({
             'source': barcode.source.name,
             'barcode': barcode.barcode,
             'uuid': str(barcode.uuid),
-            'errors': errors,
         }))
     else:
         return HttpResponse(json.dumps({
             'source': source_string,
             'barcode': barcode_string,
-            'uuid': uuid,
+            'uuid': uuid_string,
             'errors': errors,
         }), status=422)
+
+
+@atomic
+def generate_barcode(barcode_string, uuid, source):
+    if not barcode_string:
+        while not barcode_string or Barcode.objects.filter(barcode=barcode_string).count() > 0:
+            barcode_string = source.name.upper() + str(NumberGenerator.objects.create().id)
+
+    barcode_string = barcode_string.upper()
+
+    if not uuid:
+        barcode = Barcode.objects.create(barcode=barcode_string, source=source)
+    else:
+        barcode = Barcode.objects.create(barcode=barcode_string, source=source, uuid=uuid)
+
+    return barcode
